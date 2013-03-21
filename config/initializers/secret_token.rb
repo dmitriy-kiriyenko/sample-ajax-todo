5e7b3a9cbdd15d58b6c006d017f84294b5e828a6
Dmitriy Kiriyenko <dmitriy.kiryienko@gmail.com>
2013-03-21 06:20:11 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoInterview::Application.config.secret_token = '9e881c2c2ff5fa6c4d23576af315e9d19c63d22aead0bc297ed72bf235b1069fd9c25f5a3130c48ab77a2f83eeb4ae4e0688f0f0963ff8493faf8ebd4524be65'
