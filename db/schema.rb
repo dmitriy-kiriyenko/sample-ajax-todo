fa9463997ae14239ce99093eaea80eb54a3f6266
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2013-03-22 17:20:53 +0200

    Token authentication.

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20130322143940) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "authentication_token"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
   end
